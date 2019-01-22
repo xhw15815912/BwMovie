6d48d9bc84fab0cc9e50e0f5c92f6fe8d16d480a
bulangxiong <45414734+bulangxiong@users.noreply.github.com>
2019-01-22 17:31:21 +0800

    ximhg

---
+++ app/src/main/java/xhw/bwie/com/bwmovie/ui/RegiterActivity.java
@@ -0,0 +1,15 @@
+package xhw.bwie.com.bwmovie.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import xhw.bwie.com.bwmovie.R;
+
+public class RegiterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_regiter);
+    }
+}
