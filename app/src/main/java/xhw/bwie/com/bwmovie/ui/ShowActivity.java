c88bd275565282d5a11b4399d33a0c59967f4623
bulangxiong <45414734+bulangxiong@users.noreply.github.com>
2019-01-22 17:44:57 +0800

    ximhg

---
+++ app/src/main/java/xhw/bwie/com/bwmovie/ui/ShowActivity.java
@@ -0,0 +1,15 @@
+package xhw.bwie.com.bwmovie.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import xhw.bwie.com.bwmovie.R;
+
+public class ShowActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show);
+    }
+}
