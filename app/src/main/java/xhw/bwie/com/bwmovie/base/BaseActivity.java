6108a8ea6ffa913e2ece1589be8714d500a8bcd5
xhw15815912 <1076731817@qq.com>
2019-01-22 17:51:59 +0800

    xxxqq

--- app/src/main/java/xhw/bwie/com/bwmovie/base/BaseActivity.java
+++ app/src/main/java/xhw/bwie/com/bwmovie/base/BaseActivity.java
@@ -1,5 +1,9 @@
 package xhw.bwie.com.bwmovie.base;
 
+import android.app.Dialog;
+
+import xhw.bwie.com.bwmovie.core.WDActivity;
+
 /**
  * 作者：夏洪武
  * 时间：2019/1/22.
@@ -7,4 +11,11 @@ package xhw.bwie.com.bwmovie.base;
  * 说明：
  */
 public class BaseActivity {
+    public final static int PHOTO =0;
+    public final static int CAMERA = 1;
+    public Dialog mLoadDiaLog;
+
+    private static WDActivity mFregroundActivity = null;
+
+
 }
