1f48338ea00a794e6ca256a4cfdb923862665d53
bulangxiong <45414734+bulangxiong@users.noreply.github.com>
2019-01-22 17:25:55 +0800

    ximhg

--- app/src/main/java/xhw/bwie/com/bwmovie/MainActivity.java
+++ app/src/main/java/xhw/bwie/com/bwmovie/MainActivity.java
@@ -2,6 +2,7 @@ package xhw.bwie.com.bwmovie;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        Toast.makeText(this, "长", Toast.LENGTH_SHORT).show();
 
     }
 
